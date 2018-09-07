39013c970f28c5ddb4b2a70012903c77bd017498
Allan <allanxu1310@gmail.com>
2018-09-07 21:06:57 +0800

    auth2-center init

---
+++ xlfd-cloud-auth2-center/src/main/java/com/xlfd/auth2center/repository/AccountRepository.java
@@ -0,0 +1,20 @@
+package com.xlfd.auth2center.repository;
+
+import com.xlfd.auth2center.entity.Account;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
+
+/**
+ * 账户数据库操作类
+ * MongoDB操作接口
+ */
+@Component
+public interface AccountRepository extends MongoRepository<Account, String> {
+
+    /**
+     * 根据用户名查找账户信息
+     * @param username 用户名
+     * @return 账户信息
+     */
+    Account findByUserName(String username);
+}
