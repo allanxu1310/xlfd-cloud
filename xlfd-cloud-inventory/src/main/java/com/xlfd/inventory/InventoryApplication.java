9c78694bd2352f2afa20d09cfe5a5c38e0015553
Allan <allanxu1310@gmail.com>
2018-09-09 20:07:31 +0800

    通过网关认证示例

--- xlfd-cloud-inventory/src/main/java/com/xlfd/inventory/InventoryApplication.java
+++ xlfd-cloud-inventory/src/main/java/com/xlfd/inventory/InventoryApplication.java
@@ -1,11 +1,11 @@
 package com.xlfd.inventory;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryApplication {
 
 	public static void main(String[] args) {
